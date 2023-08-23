2051d05842d19f191fecaa2c95620c37e81753e1
Ramaz Artmeladze <ramaz.artmeladze@outlook.com>
2023-08-24 00:29:08 +0400

    update

--- src/main/java/school/mjc/stage0/conditions/task4/TernaryOperator.java
+++ src/main/java/school/mjc/stage0/conditions/task4/TernaryOperator.java
@@ -3,7 +3,7 @@ package school.mjc.stage0.conditions.task4;
 public class TernaryOperator {
     public static void main(String[] args) {
         int b = 16;
-        b = b > 0 ? 10 : 23;  // Use the ternary operator to assign value based on the condition
+        b = b < 0 ? 10 : 23;  // Use the ternary operator to assign value based on the condition
         System.out.println(b); // Output: 23
     }
 }
